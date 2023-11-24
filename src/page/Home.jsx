1e6d4d2835dd0e7bcba745745a29f8526c76d1c5
Sebastian <practecnologia03@cec.comfenalco.edu.co>
2023-11-24 17:05:59 -0500

    adelantos responsive

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,36 +1,64 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+import CV from "../assets/CV.pdf";
 
 function Home() {
+    const { setMostrarModal } = useContext(AppContext);
+
     return (
         <div className="flex justify-center">
-            <div className="inline-flex flex-col sm:flex-row justify-center items-center mt-[20vh]">
-                <div className='sm:w-2/5'>
-                    <div className=''>
-                        <div className="gap-3 mb-3">
-                            <h1 className='text-white text-5xl font-medium max-sm:text-6xl max-sm:text-center'>
-                                Hola a <span className='text-blue-500 font-bold inline-flex items-center'> Tod@s!
-                                    <div className='inline-block w-14 ml-3 max-sm:w-14'>
-                                        <img src='./media/emojiMano.png' alt='Emoji de Mano' />
-                                    </div>
-                                </span>
-                            </h1>
-                        </div>
+            <div className="inline-flex flex-col lg:flex-row justify-center items-center mt-[20vh] mx-auto">
+                <div className="max-sm:w-10/12 max-md:w-2/3 max-lg:w-2/3 mb-6 max-lg:mr-6">
+                    <div className="mb-1 ">
+                        <h1 className="text-white text-5xl font-medium max-sm:text-4xl max-sm:text-center max-md:text-4xl max-md:text-center max-lg:text-center">
+                            Hola a{" "}
+                            <span className="text-blue-500 font-bold inline-flex items-center">
+                                Tod@s!
+                                <div className="inline-block w-14 ml-3 max-sm:w-14">
+                                    <img src="./media/emojiMano.png" alt="Emoji de Mano" />
+                                </div>
+                            </span>
+                        </h1>
                     </div>
-                    <div className='flex justify-center'>
-                        <div className='mb-3 max-sm:w-[85%] max-sm:text-center'>
-                            <p className='text-white text-[25px] max-sm:text-lg sm:w-10/12'>
-                                Me gusta la programación, los videojuegos, la música,
-                                pero sobre todo pasarla bien con lo que hago.
+
+                    <div className="max-sm:justify-center max-sm:flex">
+                        <div className="flex justify-center mb-2 max-md:text-center max-sm:text-center max-lg:text-center">
+                            <p className="text-white text-xl max-sm:text-lg">
+                                Me gusta la programación, los videojuegos, la música, pero sobre todo pasarla bien con lo que hago.
                             </p>
                         </div>
                     </div>
+
                 </div>
-                <div className='w-1/4'>
-                    <img className='border border-white/60 shadow-lg rounded-full max-sm:hidden max-md:hidden' src="./media/perfil2.png" alt="Foto personal de 'presentacion'" />
+                <div className="max-lg:w-1/3">
+                    <img
+                        className="border-white/60 shadow-lg rounded-full max-sm:hidden max-md:hidden "
+                        src="./media/perfil2.png"
+                        alt="Foto personal de 'presentacion'"
+                    />
+                </div>
+                <div className="md:hidden">
+                    <button
+                        className="text-base font-medium text-white rounded-lg p-1.5 px-4 bg-blue-500 ease-in duration-300 hover:bg-[#0cbeff] active:bg-[#0cbeff]"
+                        onClick={() => setMostrarModal(true)}
+                    >
+                        Contactame
+                    </button>
+                </div>
+                <div className="md:hidden mt-4">
+                    <a
+                        href={CV}
+                        download="Sebastian Chico - Front-end Developer"
+                        rel="noreferrer"
+                        target="_blank"
+                        className="text-base font-medium text-white rounded-lg p-1.5 px-4 bg-blue-500 ease-in duration-300 hover:bg-[#0cbeff] active:bg-[#0cbeff]"
+                    >
+                        Descargar CV
+                    </a>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default Home;
--- src/page/Proyectos.jsx
+++ src/page/Proyectos.jsx
@@ -3,21 +3,6 @@ import CardProject from '../components/CardProject';
 
 function Proyectos() {
 
-    const descripcion = [
-        "Crud 'To do List' que permite Agregar, Editar y Eliminar tareas que las almacena utilizando ",
-        <a key="pocketBase" href="https://pocketbase.io/" target="_blank" rel="noreferrer">
-            <strong className='text-blue-500 ease-in duration-300 hover:text-[#0cbeff]'>PocketBase</strong>
-        </a>,
-        " como BackEnd. Realizada con React, Typescript, Bootstrap."
-    ]
-
-    const descripcionPoke = [
-        "Pokedex que contiene filtrado, detalle de cada Pokemon. consumo de la API, ",
-        <a key="pokemonApi" href="https://pokeapi.co/" target="_blank" rel="noreferrer">
-            <strong className='text-[#ffc04b] ease-in duration-300 hover:text-[#ffed4b]'>PokeAPI</strong>
-        </a>, ". realizada con React, Typescript, Tailwind CSS"
-    ]
-
     return (
         <>
             <div className="flex flex-wrap gap-5 justify-center items-center mt-16 mb-[5%]">
